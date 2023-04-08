a507c7a7f7a0f3af2ad634e838a792df21bbc419
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-04-08 15:28:07 -0300

    feat - implementação inicial do contador, inserindo inputs e atualizando estilos

--- src/script.js
+++ src/script.js
@@ -22,31 +22,20 @@ const dayOfTheWeek = [
   "Quarta-feira",
   "Quinta-feira",
   "Sexta-feira",
-  "Sábado"
+  "Sábado",
 ];
 
 setInterval(() => {
   const data = new Date();
-
   let diaDaSemana = dayOfTheWeek[data.getDay()];
 
   let hour = data.getHours();
   let minute = data.getMinutes();
   let second = data.getSeconds();
 
-  if (hour <= 9) {
-    hour = `0${data.getHours()}`;
-  }
-
-  if (minute <= 9) {
-    minute = `0${data.getMinutes()}`;
-  }
-
-  if (second <= 9) {
-    second = `0${data.getSeconds()}`;
-  }
-
-  let time = `${hour}:${minute}:${second}h`;
+  let time = `${hour <= 9 ? `0${hour}` : hour}:${
+    minute <= 9 ? `0${minute}` : minute
+  }:${second <= 9 ? `0${second}` : second}h`;
 
   const day = data.getDate();
   const month = monthsOfTheYear[data.getMonth()];
@@ -56,3 +45,48 @@ setInterval(() => {
 
   hoursAndYear.innerText = `${diaDaSemana}, ${time} - ${dataDeHoje}`;
 }, 1000);
+
+const cronometro = document.querySelector("#stopwatch");
+
+let horas = 0;
+let minutos = 1;
+let segundos = 1;
+
+setInterval(() => {
+  if (!horas && !minutos && !segundos) {
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+  } else {
+    // Segundos decremento
+    segundos -= 1;
+  }
+  
+  // Segundos
+  if (segundos === -1) {
+    segundos = 59;
+  }
+
+  // Minutos decremento
+  if (segundos >= 59) {
+    minutos -= 1;
+  }
+
+  // Minutos
+  if(minutos === -1 && horas >= 0) {
+    minutos = 59;
+  }
+
+  // Horas decremento
+  if (minutos >= 59 && horas >= 1) {
+    horas -= 1
+  }
+
+
+  
+  cronometro.innerText = `${horas <= 9 ? `0${horas}` : horas}:${
+    minutos <= 9 ? `0${minutos}` : minutos
+  }:${segundos <= 9 ? `0${segundos}` : segundos}`;
+}, 1000);
+
+
