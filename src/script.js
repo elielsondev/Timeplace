4522f51be597779f6ef83341238de6d99d9dabad
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-04-12 18:15:59 -0300

    feat - finalizando lógica do contador, implementação de um favicon e uma imagem no plano de fundo

--- src/script.js
+++ src/script.js
@@ -48,45 +48,86 @@ setInterval(() => {
 
 const cronometro = document.querySelector("#stopwatch");
 
-let horas = 0;
-let minutos = 1;
-let segundos = 1;
+let h = document.querySelector("#hours");
+let m = document.querySelector("#minutes");
+let s = document.querySelector("#seconds");
 
-setInterval(() => {
-  if (!horas && !minutos && !segundos) {
-    horas = 0;
-    minutos = 0;
-    segundos = 0;
-  } else {
-    // Segundos decremento
-    segundos -= 1;
-  }
-  
-  // Segundos
-  if (segundos === -1) {
-    segundos = 59;
-  }
+const buttonStart = document.querySelector("#btn-start");
+const buttonStop = document.querySelector("#btn-stop");
 
-  // Minutos decremento
-  if (segundos >= 59) {
-    minutos -= 1;
-  }
+let horas;
+let minutos;
+let segundos;
 
-  // Minutos
-  if(minutos === -1 && horas >= 0) {
-    minutos = 59;
-  }
+const startFunction = (e) => {
+  e.preventDefault();
 
-  // Horas decremento
-  if (minutos >= 59 && horas >= 1) {
-    horas -= 1
-  }
+  horas = h.value || 0;
+  minutos = m.value || 0;
+  segundos = s.value || 0;
 
+  h.value = null;
+  m.value = null;
+  s.value = null;
 
-  
-  cronometro.innerText = `${horas <= 9 ? `0${horas}` : horas}:${
-    minutos <= 9 ? `0${minutos}` : minutos
-  }:${segundos <= 9 ? `0${segundos}` : segundos}`;
-}, 1000);
+  let intervaloStart = setInterval(() => {
+    // Desativa o botão assim que algum valor for inserido e iniciada a contagem:
+    if (
+      typeof horas === "number" ||
+      typeof minutos === "number" ||
+      typeof segundos === "number"
+    ) {
+      buttonStart.disabled = true;
+      buttonStart.style.backgroundColor = "red";
+    }
 
+    // Decremento segundos:
+    if (segundos >= 0 && cronometro.textContent !== "00:00:00") {
+      buttonStart.disabled = true;
+      buttonStart.style.backgroundColor = "red";
+      segundos -= 1;
+    }
 
+    // Gerando 59 segundos:
+    if (segundos < 0 || (segundos === -1 && minutos > 0)) {
+      segundos = 59;
+    }
+
+    // Decremento minutos:
+    if (minutos > 0 && segundos === 59) {
+      minutos -= 1;
+    }
+
+    // Gerando 59 minutos:
+    if (horas >= 1 && minutos === 0 && segundos === 59) {
+      segundos = 59;
+      minutos = 59;
+    }
+
+    // Decremento horas:
+    if (horas > 0 && minutos === 59) {
+      horas -= 1;
+    }
+
+    cronometro.innerText = `${horas <= 9 ? `0${horas}` : horas}:${
+      minutos <= 9 ? `0${minutos}` : minutos
+    }:${segundos <= 9 ? `0${segundos}` : segundos}`;
+
+    // Ativando o botão e parando o setInterval:
+    if (cronometro.textContent === "00:00:00") {
+      buttonStart.disabled = false;
+      buttonStart.style.backgroundColor = "rgb(5, 214, 5)";
+      clearInterval(intervaloStart);
+    }
+  }, 1000);
+};
+
+buttonStart.addEventListener("click", startFunction);
+
+// if (horas === 0 && minutos === 0 && segundos === 0) {
+//   window.alert('Acabou!!')
+//   buttonStart.style.backgroundColor = rgb(5, 214, 5);
+//   buttonStart.disabled = false;
+//   // Fonte: https://horadecodar.com.br/2021/04/20/como-parar-um-setinterval-em-javascript/
+//   clearInterval(intervaloStart);
+// }
